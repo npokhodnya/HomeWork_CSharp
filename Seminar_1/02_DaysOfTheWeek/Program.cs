27f3d78c7e49c1df37eb9dda27ce0a221c3e96ff
Nikita <nikitapohod@icloud.com>
2022-08-10 21:21:01 +0300

    Task 02 Fix

--- Seminar_1/02_DaysOfTheWeek/Program.cs
+++ Seminar_1/02_DaysOfTheWeek/Program.cs
@@ -2,13 +2,6 @@
 
 Console.Write("Введите номер дня недели: ");
 int number = int.Parse(Console.ReadLine());
-
-// У программистов всё с нуля начинается :)
-if ((number == 0) || (number  == 1)) Console.Write($"День недели под номером {number} - это ПОНЕДЕЛЬНИК!");   
-else if (number == 2) Console.Write($"День недели под номером {number} - это ВТОРНИК!");
-else if (number == 3) Console.Write($"День недели под номером {number} - это СРЕДА!");
-else if (number == 4) Console.Write($"День недели под номером {number} - это ЧЕТВЕРГ!");
-else if (number == 5) Console.Write($"День недели под номером {number} - это ПЯТНИЦА!");
-else if (number == 6) Console.Write($"День недели под номером {number} - это СУББОТА!");
-else if (number == 8) Console.Write($"День недели под номером {number} - это ВОСКРЕСЕНЬЕ!");
-else if ((number < 0) || (number  > 8)) Console.Write($"Число {number} НЕ является номером дня недели!");
+string[] array = new string[] {"ПОНЕДЕЛЬНИК", "ВТОРНИК", "СРЕДА", "ЧЕТВЕРГ", "ПЯТНИЦА", "СУББОТА", "ВОСКРЕСЕНЬЕ"};
+if ((number >= 1) && (number < 8)) Console.Write($"День недели под номером {number} - это {array[number-1]}!");
+else Console.Write($"Число {number} НЕ является номером дня недели!");
