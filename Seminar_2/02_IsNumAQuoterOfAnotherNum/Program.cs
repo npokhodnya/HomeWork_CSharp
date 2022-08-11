8f8a8ef3e340f5be439dd6efb4f4a3c45877959a
Nikita <nikitapohod@icloud.com>
2022-08-11 17:31:00 +0300

    Task 02 completed

---
+++ Seminar_2/02_IsNumAQuoterOfAnotherNum/Program.cs
@@ -0,0 +1,9 @@
+﻿// 2. По двум заданным числам проверять является ли одно квадратом другого.
+
+Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+if (second*second == first) Console.Write($"Число {first} является квадратом числа {second}! ({first} = {second}^2)");
+else if (first*first == second) Console.Write($"Число {second} является квадратом числа {first}! ({second} = {first}^2)");
+else Console.Write($"Ни одно число не является квадратом другого! ({first} != {second}^2 ; {second} != {first}^2)");
