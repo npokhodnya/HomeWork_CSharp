06225493712ecf4056f41f755fdd4d0d76d5c691
Nikita <nikitapohod@icloud.com>
2022-08-12 23:16:34 +0300

    Task 05 completed

---
+++ Seminar_2/05_PrintNumbersFrom_1_ToN_InThreeDegree/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N.
+
+void CheckNumsInThreeDegree(int number)
+{
+    Console.Write("Все числа (уже в 3 степени!) по порядку: ");
+    for (int i = 1; i <= number; i++) Console.Write($"{Math.Pow(i, 3)} ");
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+CheckNumsInThreeDegree(num);
