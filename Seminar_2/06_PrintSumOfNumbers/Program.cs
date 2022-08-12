ed28366e8ccdf83de9fcd0caaf6d7e7b5850645b
Nikita <nikitapohod@icloud.com>
2022-08-12 23:25:59 +0300

    Task 06 completed

---
+++ Seminar_2/06_PrintSumOfNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму чисел от 1 до А.
+
+void SumOfNums(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++) sum+=i;
+    Console.Write($"Сумма чисел от 1 до {number} = {sum}");
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+SumOfNums(num);
