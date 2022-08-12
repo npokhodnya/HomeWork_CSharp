66c93b8983c3eae7fa37de29957e93a5ed768c93
Nikita <nikitapohod@icloud.com>
2022-08-12 23:39:54 +0300

    Task 07 completed

---
+++ Seminar_2/07_CountOfDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿// Определить количество цифр в числе.
+
+void CountOfDigits(int number)
+{
+    int count = 0;
+    int dublicateNum = number;
+    while (dublicateNum>0)
+    {
+        dublicateNum/=10; 
+        count++;
+    }
+    Console.Write($"Количество цифр в числе {number} = {count}");
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+CountOfDigits(num);
