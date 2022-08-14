407bab471804cf96b1b5986235f3afd2e92faf03
Nikita <nikitapohod@icloud.com>
2022-08-14 15:58:21 +0300

    Fix orthography

--- Seminar_2/03_ShowRangesForPossibleCoordinates/Program.cs
+++ Seminar_2/03_ShowRangesForPossibleCoordinates/Program.cs
@@ -3,13 +3,13 @@
 void CheckPlane(int plane)
 {
     Random rand = new Random();
-    if (plane == 1) Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y>0), например ({rand.Next(1, 10)}; {rand.Next(1, 10)})");
-    else if (plane == 2) Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y>0), например ({rand.Next(-10, 0)}; {rand.Next(1, 10)})");
-    else if (plane == 3) Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y<0), например ({rand.Next(-10, 0)}; {rand.Next(-10, 0)})");
-    else if (plane == 4) Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y<0), например ({rand.Next(1, 10)}; {rand.Next(-10, 0)})");
-    else Console.Write($"Число {plane} не является номером плоскости!");
+    if (plane == 1) Console.Write($"Для {plane} четверти характерен диапозон координат (x>0; y>0), например ({rand.Next(1, 10)}; {rand.Next(1, 10)})");
+    else if (plane == 2) Console.Write($"Для {plane} четверти характерен диапозон координат (x<0; y>0), например ({rand.Next(-10, 0)}; {rand.Next(1, 10)})");
+    else if (plane == 3) Console.Write($"Для {plane} четверти характерен диапозон координат (x<0; y<0), например ({rand.Next(-10, 0)}; {rand.Next(-10, 0)})");
+    else if (plane == 4) Console.Write($"Для {plane} четверти характерен диапозон координат (x>0; y<0), например ({rand.Next(1, 10)}; {rand.Next(-10, 0)})");
+    else Console.Write($"Число {plane} не является номером четверти!");
 }
 Console.Clear();
-Console.Write("Введите номер плоскости: ");
+Console.Write("Введите номер четверти: ");
 int plane = int.Parse(Console.ReadLine());
 CheckPlane(plane);
