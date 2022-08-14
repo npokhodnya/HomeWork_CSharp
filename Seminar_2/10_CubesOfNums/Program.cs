2334c5d616fd5b365d882b2e3315c53279138a8e
Nikita <nikitapohod@icloud.com>
2022-08-14 15:37:58 +0300

    Task 10 completed

---
+++ Seminar_2/10_CubesOfNums/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void CheckCubesOfNums(int[] numbers)
+{
+    foreach (int element in numbers)
+        if (element%10%2 == 0) Console.Write($"{element*element*element} ");
+}
+void PrintArray(int[] array)
+{
+    int count = 0;
+    foreach (int element in array) Console.Write($"{element} ");
+}
+Console.Clear();
+var rand = new Random();
+int[] array = new int[] {rand.Next(1, 10), rand.Next(1, 10), rand.Next(1, 10), rand.Next(1, 10), rand.Next(1, 10)};
+Console.Write("Список всех элементов - ");
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Список кубов - ");
+CheckCubesOfNums(array);
+
