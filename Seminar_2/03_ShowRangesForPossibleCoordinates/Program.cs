1ef9898ea0dd198a60209fd16e3fadc5230cf2cf
Nikita <nikitapohod@icloud.com>
2022-08-14 15:48:52 +0300

    Task 03 fix

--- Seminar_2/03_ShowRangesForPossibleCoordinates/Program.cs
+++ Seminar_2/03_ShowRangesForPossibleCoordinates/Program.cs
@@ -1,26 +1,15 @@
-﻿// 3. Задать номер четверти, показать диапазоны для возможных координат.
+﻿// 2. Задать номер четверти, показать диапазоны для возможных координат.
 
+void CheckPlane(int plane)
+{
+    Random rand = new Random();
+    if (plane == 1) Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y>0), например ({rand.Next(1, 10)}; {rand.Next(1, 10)})");
+    else if (plane == 2) Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y>0), например ({rand.Next(-10, 0)}; {rand.Next(1, 10)})");
+    else if (plane == 3) Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y<0), например ({rand.Next(-10, 0)}; {rand.Next(-10, 0)})");
+    else if (plane == 4) Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y<0), например ({rand.Next(1, 10)}; {rand.Next(-10, 0)})");
+    else Console.Write($"Число {plane} не является номером плоскости!");
+}
+Console.Clear();
 Console.Write("Введите номер плоскости: ");
 int plane = int.Parse(Console.ReadLine());
-Random rand = new Random();
-if (plane == 1) 
-{
-    int x = rand.Next(1, 10); int y = rand.Next(1, 10);
-    Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y>0), например ({x}; {y})");
-}
-else if (plane == 2)
-{
-    int x = rand.Next(-10, 0); int y = rand.Next(1, 10);
-    Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y>0), например ({x}; {y})");
-}
-else if (plane == 3)
-{
-    int x = rand.Next(-10, 0); int y = rand.Next(-10, 0);
-    Console.Write($"Для {plane} плоскости характерен диапозон координат (x<0; y<0), например ({x}; {y})");
-}
-else if (plane == 4)
-{
-    int x = rand.Next(1, 10); int y = rand.Next(-10, 0);
-    Console.Write($"Для {plane} плоскости характерен диапозон координат (x>0; y<0), например ({x}; {y})");
-}
-else Console.Write($"Число {plane} не является номером плоскости!");
+CheckPlane(plane);
