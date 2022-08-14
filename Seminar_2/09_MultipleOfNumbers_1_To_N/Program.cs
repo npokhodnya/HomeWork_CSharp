fbe195d81a93800d50ba96855926599c2f38862d
Nikita <nikitapohod@icloud.com>
2022-08-14 15:05:52 +0300

    Task 09 completed

---
+++ Seminar_2/09_MultipleOfNumbers_1_To_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// 9. Написать программу вычисления произведения чисел от 1 до N.
+
+void MultiplyOfNums(int number)
+{
+    int multiply = 1;
+    for (int i = 1; i <= number; i++) multiply*=i;
+    Console.Write($"Произведение чисел от 1 до {number} = {multiply}");
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+MultiplyOfNums(num);
