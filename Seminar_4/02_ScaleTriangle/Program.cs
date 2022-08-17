c03f1bd5950f93327c10c8f95d7198c3bf993163
Nikita <nikitapohod@icloud.com>
2022-08-17 17:31:33 +0300

    Task 02 completed

---
+++ Seminar_4/02_ScaleTriangle/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2. Написать программу масштабирования фигуры.
+
+void CheckTriangle()
+{
+    Console.Write("Введите коэффициент масштабирования: ");
+    int k = int.Parse(Console.ReadLine());
+    Console.Write("Введите координаты через запятую разделяя пробелом (пример - 2,3 4,3 6,7 4,5): ");
+    string parametrs = Console.ReadLine();
+    int ax = int.Parse(parametrs[0].ToString());
+    int ay = int.Parse(parametrs[2].ToString());
+    int bx = int.Parse(parametrs[4].ToString());
+    int by = int.Parse(parametrs[6].ToString());
+    int cx = int.Parse(parametrs[8].ToString());
+    int cy = int.Parse(parametrs[10].ToString());
+    int dx = int.Parse(parametrs[12].ToString());
+    int dy = int.Parse(parametrs[14].ToString());
+    Console.Write($"Результат: ({ax*k}, {ay*k}) ({bx*k}, {by*k}) ({cx*k}, {cy*k}) ({dx*k}, {dy*k})");
+}
+Console.Clear();
+CheckTriangle();
