e3e238ed97f65bc38a37e2e5eaa2dfd79ffc95ed
Nikita <nikitapohod@icloud.com>
2022-08-17 17:41:35 +0300

    Task 03 completed

---
+++ Seminar_4/03_CopyMassive/Program.cs
@@ -0,0 +1,28 @@
+﻿// 3. Написать программу масштабирования фигуры.
+
+void PrintArray(int[] array)
+{
+    foreach (var element in array) Console.Write($"{element} ");
+    Console.WriteLine();
+}
+void FillArray(int[] array, int min, int max)
+{
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++) 
+        array[i] = rand.Next(min, max+1);
+}
+int[] CopyMassive(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++) arrayCopy[i] = array[i];
+    return arrayCopy;
+}
+Console.Clear();
+Console.Write("Введите длину массива: ");
+int len = int.Parse(Console.ReadLine());
+int[] array = new int[len];
+FillArray(array, 10, 99); 
+Console.Write("Массив: ");
+PrintArray(array);
+Console.Write("Копия массива: ");
+PrintArray(CopyMassive(array));
