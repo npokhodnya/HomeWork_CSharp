b1443b89e8678d735613f00e13b61a812c04bbda
Nikita <nikitapohod@icloud.com>
2022-08-18 16:49:20 +0300

    Fix output, round 3 digits

--- Seminar_4/02_ScaleTriangle/Program.cs
+++ Seminar_4/02_ScaleTriangle/Program.cs
@@ -12,7 +12,7 @@ void CheckTriangle()
     Console.Write($"Результат: ");
     for (int i = 0; i < parametrs.Length; i+=2)
     {
-        Console.Write($"({double.Parse(parametrs[i]) * k}, {double.Parse(parametrs[i+1]) * k}) ");
+        Console.Write($"({Math.Round(double.Parse(parametrs[i]) * k, 3)}, {Math.Round(double.Parse(parametrs[i+1]) * k, 3)}) ");
     }
 }
 Console.Clear();
