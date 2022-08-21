6edb6e02265e5b5c7006a23b7d09991e135bd6dc
Nikita <nikitapohod@icloud.com>
2022-08-21 22:21:14 +0300

    Fix 01

--- Seminar_4/01_Point/Program.cs
+++ Seminar_4/01_Point/Program.cs
@@ -1,17 +1,23 @@
 ﻿// 1. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы.
 
-string CheckPoint(int k1, int k2, int b1, int b2)
+string CheckPoint(double k1, double k2, double b1, double b2)
 {
-    if(k1 == k2) return "Данные прямы не имеют общих точек или имеют бесконечное количество общих точек!";
-    else return $"({(b2 - b1) / (k1 - k2)}, {(k1 * (b2 - b1) / (k1 - k2)) + b1})";
+    if((k1 == k2) && (b1 == b2)) return "Данные прямые не имеют общих точек!";
+    else if(k1 == k2) return "Данные прямые имеет бесконечное количество общих точек!";
+    else 
+    {
+        double x = (-b2 + b1)/(-k1 + k2);
+        double y = k2 * x + b2;
+        return $"({x}; {y})";
+    }
 }
 Console.Clear();
-Console.Write("Введите k1: ");
-int k1 = int.Parse(Console.ReadLine());
-Console.Write("Введите k2: ");
-int k2 = int.Parse(Console.ReadLine());
 Console.Write("Введите b1: ");
-int b1 = int.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
 Console.Write("Введите b2: ");
-int b2 = int.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
 Console.Write($"Результат: {CheckPoint(k1, k2, b1, b2)}");
