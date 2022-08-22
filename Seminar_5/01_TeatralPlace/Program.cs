510b9353b7a1bab22114ab2a9aaed3e7f2ee4914
Nikita <nikitapohod@icloud.com>
2022-08-22 20:15:29 +0300

    Task 01 done

---
+++ Seminar_5/01_TeatralPlace/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1. Театральная площадь в столице Берляндии представляет собой прямоугольник n × m метров. 
+// Какое наименьшее количество плит понадобится для замощения площади? 
+
+string[] parametres = Console.ReadLine().Split(" ");
+int n = int.Parse(parametres[0]);
+int m = int.Parse(parametres[1]);
+int a = int.Parse(parametres[2]);
+decimal h1 = (n + a - 1) / a;
+decimal h2 = (m + a - 1) / a;
+Console.WriteLine(h1*h2);
