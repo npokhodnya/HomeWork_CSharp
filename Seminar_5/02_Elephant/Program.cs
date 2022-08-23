7ebcd15e3cde288e1d7b8a005b7616ae59b739be
Nikita <nikitapohod@icloud.com>
2022-08-23 10:13:11 +0300

    Task 02 done

---
+++ Seminar_5/02_Elephant/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2. Слоник решил сходить в гости к другу. 
+// Помогите ему определить, за какое минимальное количество шагов он может добраться до дома друга.
+
+var x = double.Parse(Console.ReadLine());
+var res = x/5;
+if (res%1 > 0) Console.WriteLine((int)res+1);
+else Console.WriteLine(res);
