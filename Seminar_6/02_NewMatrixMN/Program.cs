a0e4552bd7bd43fee275821cb3067ab2be77996f
Nikita <nikitapohod@icloud.com>
2022-08-27 13:55:10 +0300

    Taask 02 sloved

---
+++ Seminar_6/02_NewMatrixMN/Program.cs
@@ -0,0 +1,28 @@
+﻿// 2. Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+double[,] CreateMatrix(int row, int col)
+{
+    var matrix = new double[row, col];
+    var rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <  matrix.GetLength(1); j++) matrix[i, j] = i+j;
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите число столбцов: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Введите число сторк: ");
+var m = int.Parse(Console.ReadLine());
+Console.WriteLine();
+var matr = CreateMatrix(m, n);
+PrintMatrix(matr);
