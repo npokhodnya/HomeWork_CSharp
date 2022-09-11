40993bdb481efb243243607614f433befc32a7a6
Nikita <nikitapohod@icloud.com>
2022-09-11 18:52:07 +0300

    Task 07 sloved

---
+++ Seminar_9/07_FistN_Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int N = 10;
+int a = 3;
+int b = 5;
+
+int CheckNumber(int a, int b, int position)
+{
+    if (position == 1) return a;
+    if (position == 2) return b;
+    return CheckNumber(a, b, position - 2) + CheckNumber(a, b, position - 1);
+}
+
+void ShowNumbers(int count, int nowPosition = 0)
+{
+    nowPosition++;
+    if(count >= nowPosition)
+    {
+        Console.Write($"{CheckNumber(a, b, nowPosition)} ");
+        ShowNumbers(count, nowPosition);
+    }
+    else return; 
+}
+
+ShowNumbers(5); // 3 5 8 13 21  --->  3, 5, (3 + 5), (3 + 5 + 5), (3 + 5 + 5 + (3 + 5)
