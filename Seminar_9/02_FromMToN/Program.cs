c9105bfe8d9799ef55633a2208ba0f767d75d18e
Nikita <nikitapohod@icloud.com>
2022-09-11 17:33:25 +0300

    Task 02 sloved

---
+++ Seminar_9/02_FromMToN/Program.cs
@@ -0,0 +1,11 @@
+﻿void From_M_To_N_Rec(int m, int n)
+{
+    if(m<=n) 
+    {
+        Console.Write($"{m} ");
+        From_M_To_N_Rec(n, m+1);
+    }
+    else return;
+}
+
+From_M_To_N_Rec(10, 5); // 5 6 7 8 9 10 
