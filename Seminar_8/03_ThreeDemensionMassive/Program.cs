eedcad36713184af890775fe7e8d727527ea7553
Nikita <nikitapohod@icloud.com>
2022-09-11 11:58:33 +0300

    Tasks sloved

---
+++ Seminar_8/03_ThreeDemensionMassive/Program.cs
@@ -0,0 +1,68 @@
+﻿// 3. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента.
+
+void FillMassive(int[,,] array, int min, int max)
+{
+    var rand = new Random();
+    int num = rand.Next(min, max+1);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) 
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                while (true) 
+                {
+                    if (!IsNumInArray(array, num)) 
+                    {
+                        array[i, j, k] = num;
+                        break;
+                    }
+                    num = rand.Next(min, max+1);
+                } 
+            }
+        }
+    }
+}
+void PrintMatrix(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++) 
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} - [{i}, {j}, {k}]");
+                Console.WriteLine();
+            } 
+            
+        }
+    }
+}
+bool IsNumInArray(int[, ,] matrix, int num)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j <  matrix.GetLength(1); j++) 
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (matrix[i, j, k] == num)
+                    return true;
+            } 
+        }
+    }
+    return false;
+}
+Console.Clear();
+Console.Write("Enter 0 demension: ");
+var n = int.Parse(Console.ReadLine());
+Console.Write("Enter 1 demension: ");
+var m = int.Parse(Console.ReadLine());
+Console.Write("Enter 2 demension: ");
+var b = int.Parse(Console.ReadLine());
+int[,,] massive = new int[m, n, b];
+FillMassive(massive, 10, 99);
+Console.WriteLine("Матрица: ");
+PrintMatrix(massive);
+Console.WriteLine();
+
---
+++ Seminar_8/04_Pascal/Program.cs
@@ -0,0 +1,17 @@
+﻿int no_row,c=1,blk,i,j;
+Console.WriteLine("Input number of rows: ");
+no_row = int.Parse(Console.ReadLine());
+for(i=0;i<no_row;i++)
+{
+    for(blk=1;blk<=no_row-i;blk++)
+    Console.Write(" ");
+    for(j=0;j<=i;j++)
+    {
+        if (j==0||i==0)
+            c=1;
+        else
+            c=c*(i-j+1)/j;
+        Console.Write(" "+c);
+    }
+    Console.Write("\n");
+}
