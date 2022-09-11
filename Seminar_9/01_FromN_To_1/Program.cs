bf4b4045cfb0a2ee63ac701476af8b869558d538
Nikita <nikitapohod@icloud.com>
2022-09-11 17:29:54 +0300

    Task 01 sloved

---
+++ Seminar_9/01_FromN_To_1/Program.cs
@@ -0,0 +1,11 @@
+﻿void From_N_To_1_Rec(int n, int finish=1)
+{
+    if(finish<=n) 
+    {
+        Console.Write($"{n} ");
+        From_N_To_1_Rec(n-1, finish);
+    }
+    else return;
+}
+
+From_N_To_1_Rec(10);
